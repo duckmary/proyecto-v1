fc088f66063a754685e6c1a429d06c08f4f99681
Fran Ruiz <panchoruizlopez89@gmail.com>
2026-02-19 10:42:21 +0100

    Agregando error

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-console.log('Proyecto V1 iniciado');
+console.log("Proyecto V1 iniciado");
 
 // Función simple de ejemplo
 function saludar() {
-    alert('¡Hola desde mi proyecto V1!');
+  alert("¡Hola desde mi proyecto V1!");
 }
 
 // Llamar a la función cuando cargue la página
-window.onload = saludar;
+wind.onlad = saludar;
