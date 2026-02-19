4b6d28dea77fd9e0d3f9e2de2ea47f3f7baef485
Fran Ruiz <panchoruizlopez89@gmail.com>
2026-02-19 10:43:36 +0100

    Agregando error

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Proyecto V1 iniciado");
 // Función simple de ejemplo
 function saludar() {
   alert("¡Hola desde mi proyecto V1!");
-}
+
 
 // Llamar a la función cuando cargue la página
 wind.onlad = saludar;
